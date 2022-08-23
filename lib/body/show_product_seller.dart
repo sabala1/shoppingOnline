6e06802cc1ad52cf679fb281771832c0ac61e602
sabala1 <101605968+sabala1@users.noreply.github.com>
2022-08-24 04:47:15 +0700

    show Text & image on ListView
    แสดงข้อความ และรูปภาพ ที่ได้จาก API

--- lib/body/show_product_seller.dart
+++ lib/body/show_product_seller.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shoppingonline/models/product.dart';
 import 'package:shoppingonline/universes/constant.dart';
+import 'package:shoppingonline/widgets/show_img.dart';
 import 'package:shoppingonline/widgets/show_progress.dart';
 import 'package:shoppingonline/widgets/show_title.dart';
 
@@ -18,6 +20,7 @@ class ShowProductSeller extends StatefulWidget {
 class _ShowProductSellerState extends State<ShowProductSeller> {
   bool load = true;
   bool? haveData;
+  List<ProductModel> productModels = [];
 
   @override
   void initState() {
@@ -53,6 +56,7 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
             setState(() {
               load = false;
               haveData = true;
+              productModels.add(model);
             });
           }
         }
@@ -62,14 +66,95 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
 
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
       body: load
           ? ShowProgress()
           : haveData!
-              ? Text('Have Data')
+              ? ListView.builder(
+                  itemCount: productModels.length,
+                  itemBuilder: (context, index) => Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(4),
+                          width: size * 0.5 - 4,
+                          height: size * 0.5,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ShowTitle(
+                                title: productModels[index].name,
+                                textStyle: MyConstant().b2Style(),
+                              ),
+                              Container(
+                                width: size * 0.5,
+                                height: size * 0.4,
+                                child: CachedNetworkImage(
+                                  fit: BoxFit.cover,
+                                  imageUrl:
+                                      createUrl(productModels[index].images),
+                                  placeholder: (context, url) => ShowProgress(),
+                                  errorWidget: (context, url, error) =>
+                                      ShowImage(pathImage: MyConstant.image1),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          padding: EdgeInsets.all(4),
+                          width: size * 0.5 - 4,
+                          height: size * 0.4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ShowTitle(
+                                title:
+                                    'Price ${productModels[index].price} THB',
+                                textStyle: MyConstant().b2Style(),
+                              ),
+                              ShowTitle(
+                                title: productModels[index].detail,
+                                textStyle: MyConstant().b3Style(),
+                              ),
+                              Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.edit_outlined,
+                                      size: 36,
+                                      color: MyConstant.blackdark,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      confirmDialogDelete(productModels[index]);
+                                    },
+                                    icon: Icon(
+                                      Icons.delete_outline,
+                                      size: 36,
+                                      color: MyConstant.blackdark,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
               : Center(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,                    
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       ShowTitle(
                         title: 'No Product',
@@ -77,12 +162,47 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
                       ),
                     ],
                   ),
-              ),
+                ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: MyConstant.pinkdark,
         onPressed: () =>
             Navigator.pushNamed(context, MyConstant.routeAddProduct),
         child: Text('Add'),
+      ),
+    );
+  }
+
+  String createUrl(String string) {
+    String result = string.substring(1, string.length - 1);
+    List<String> strings = result.split(',');
+    String url = '${MyConstant.domain}/shoppingonline${strings[0]}';
+    return url;
+  }
+
+  Future<Null> confirmDialogDelete(ProductModel productModel) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: ListTile(
+          leading: CachedNetworkImage(
+            imageUrl: createUrl(productModel.images),
+            placeholder: (context, url) => ShowProgress(),
+          ),
+          title: ShowTitle(
+            title: 'Delete ${productModel.name} ?',
+            textStyle: MyConstant().b2Style(),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Delete'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancel'),
+          ),
+        ],
       ),
     );
   }
